ff2ae7af9bbdf7d11c297c1617a3f1e18214996c
Daniel Arias <danielsam707@gmail.com>
2025-07-21 13:55:30 -0500

    Instalación inicial de Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
-use App\Http\Controllers\CategoriaController;
-use App\Http\Controllers\ProductoController;
-use App\Http\Controllers\ProveedorController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-Route::resource('categorias', CategoriaController::class);
-Route::resource('productos', ProductoController::class);
-Route::resource('proveedores', ProveedorController::class)
-->parameters(['proveedores' => 'proveedor']);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
