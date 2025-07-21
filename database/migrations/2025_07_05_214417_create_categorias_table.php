3ae3260836159cd797f671a525ed68737038472c
Daniel Arias <danielsam707@gmail.com>
2025-07-21 10:15:27 -0500

    Correccion de tabla de categorias

--- database/migrations/2025_07_05_214417_create_categorias_table.php
+++ database/migrations/2025_07_05_214417_create_categorias_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('nombre');
             $table->text('descripcion')->nullable();
-            $table->foreignId('id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
